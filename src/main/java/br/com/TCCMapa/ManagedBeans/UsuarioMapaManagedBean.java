126e1daede9e822c6c80c769c86537c4f1941dc3
Diego Trindade <Diego Trindade@DESKTOP-9BENR7D>
2021-06-12 23:24:34 -0300

    Add mapaUsuario e edit to index

--- src/main/java/br/com/TCCMapa/ManagedBeans/UsuarioMapaManagedBean.java
+++ src/main/java/br/com/TCCMapa/ManagedBeans/UsuarioMapaManagedBean.java
@@ -4,21 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.TCCMapa.dao.ManterMapaDAO;
 import br.com.TCCMapa.model.MapaUsuario;
+import br.com.TCCMapa.model.Usuario;
 
 @ManagedBean(name = "userMapMB")
 @SessionScoped
 public class UsuarioMapaManagedBean {
 	private ManterMapaDAO manterMapaDAO = new ManterMapaDAO();
 	public List<MapaUsuario> listaMapas = new ArrayList<MapaUsuario>();
+	public MapaUsuario mapaUsuario = new MapaUsuario();
 	
 	@PostConstruct
     public void init() {
 		this.listaMapas = manterMapaDAO.retornaListaMapaPorUsuario();
+    }
+	
+	public String edit(MapaUsuario mapaUsuario) {
+		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("mapaUsuario",mapaUsuario);
+		return "/index";
+	}
+	
+	public void add() {
+		manterMapaDAO.inserirMapaUsuario(mapaUsuario);
+    	FacesMessage message = new FacesMessage("Mapa "+ mapaUsuario.getNomeMapa() + " adicionado com sucesso.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        this.listaMapas = manterMapaDAO.retornaListaMapaPorUsuario();
+        this.mapaUsuario = new MapaUsuario();
+        
     }
 
 	public ManterMapaDAO getManterMapaDAO() {
--- src/main/java/br/com/TCCMapa/dao/ManterMapaDAO.java
+++ src/main/java/br/com/TCCMapa/dao/ManterMapaDAO.java
@@ -1,5 +1,9 @@
 package br.com.TCCMapa.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,8 +13,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 
+import org.hibernate.HibernateException;
+
 import br.com.TCCMapa.model.MapaUsuario;
 import br.com.TCCMapa.model.Usuario;
+import br.com.TCCMapa.utils.ConnectionManager;
 
 public class ManterMapaDAO {
 
@@ -35,6 +42,53 @@ public class ManterMapaDAO {
 	      }
 	      return null;
 	    
+	}
+	
+	public boolean inserirMapaUsuario(MapaUsuario mapaUsuario) {
+        try {
+            em.getTransaction().begin();
+            MapaUsuario newMapaUsuario = new MapaUsuario();
+            newMapaUsuario.setId(this.getNextIdMapaUsuario());  
+            newMapaUsuario.setNomeMapa(mapaUsuario.getNomeMapa());
+      	  em.persist(newMapaUsuario);
+      	  em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+              e.printStackTrace();
+              return false;
+        }
+	}
+	
+	public int getNextIdMapaUsuario() {
+		PreparedStatement ps = null;
+		try {
+			ConnectionManager connectionManager = new ConnectionManager();
+			Connection conn = connectionManager.getConnection();
+			
+			ps = conn.prepareStatement("SELECT nextVal('mapausuario_seq')");
+			ResultSet rs = ps.executeQuery();
+			if (rs != null) {
+				int nextVal = 0 ;
+			    while (rs.next()) {
+			    	nextVal = rs.getInt(1);
+			    }
+			    rs.close();
+			    return nextVal;
+			}
+			ps.close();
+
+		} catch (HibernateException ex) {
+			ex.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return 0;
 	}
 	
 }
