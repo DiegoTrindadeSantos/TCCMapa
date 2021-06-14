0e106ed6731e7aa4022352ab0037d39760781f4a
Diego Trindade <Diego Trindade@192.168.0.6>
2021-06-14 12:00:41 -0300

    ajuste

--- src/main/webapp/js/mapa.js
+++ src/main/webapp/js/mapa.js
@@ -49,7 +49,7 @@ var attribution = '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> c
 			var mapnik = L.tileLayer(
 			        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 			        , {attribution: attribution}
-			).addTo(m);
+			);
 			
 			var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 				attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
